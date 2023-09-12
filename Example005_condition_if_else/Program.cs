65a21e4f0977c618c6ffe899ea6d3b683703e401
Roman Gunger <Romangv93@gmail.com>
2023-09-12 21:36:18 +0300

    example005_if_else

---
+++ Example005_condition_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter name!");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "masha")
+{
+    Console.WriteLine("OMG MASHA");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(userName);
+}
